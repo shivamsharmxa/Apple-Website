1dd7914d34a74d78afb55f0552a1448b962800aa
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-04-11 23:44:35 +0530

    Features added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./components/Features"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
@@ -14,6 +15,7 @@ const App = ()=> {
     <Hero/>
     <Highlights/>
     <Model  />
+    <Features />
 
    </main>
   )
